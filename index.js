f4df7448009f6f78c5f2c99aaa7540797ae1d696
sheikh-areeba94 <areebamansoor9492@gmail.com>
2026-08-28 02:41:04 +0500

    Initial commit

--- index.js
+++ index.js
@@ -1,40 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import connectDB from "./config/db.js";
-// import authRoutes from "./routes/authRoutes.js";
-// import adminRoutes from "./routes/adminRoutes.js";
-// import feedbackRoutes from "./routes/feedbackRoutes.js";
-
-// dotenv.config();
-// connectDB();
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-
-// app.get("/", (req, res) => {
-//   res.send("API is running...");
-// });
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/feedback", feedbackRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -69,6 +32,11 @@ app.use("/api/feedback", feedbackRoutes);
 app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
